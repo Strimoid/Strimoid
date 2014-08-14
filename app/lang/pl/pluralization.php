a5f485cdfff86edda3af26f52fe8e4f7d37bde70
supernrm <admin@normanos.com>
2014-08-14 13:18:06 +0200

    Update pluralization.php
    comments: fix dla większych wartości + czytelność = analogicznie dla comments popraw sobie resztę w/w

--- app/lang/pl/pluralization.php
+++ app/lang/pl/pluralization.php
@@ -8,7 +8,7 @@ return array(
     'replies' => '{0} :count odpowiedzi|{1} :count odpowiedź|[2,Inf] :count odpowiedzi',
     'moderators' => '{0} :count moderatorów|{1} :count moderator|[2,Inf] :count moderatorów',
     'banned' => '{0} :count zbanowanych|{1} :count zbanowany|[2,Inf] :count zbanowanych',
-    'comments' => '{0} :count komentarzy|{1} :count komentarz|[2,4] :count komentarze|[5,21] :count komentarzy|[22,24] :count komentarze|[25,Inf] :count komentarzy',
+    'comments' => ':count komentarz|:count komentarze|:count komentarzy',
     'moderatedgroups' => '{0} :count moderowanych grup|{1} :count moderowana grupa|[2,4] :count moderowane grupy|[5,Inf] :count moderowanych grup',
     'minutes' => '{0} :count minut|{1} :count minuta|[2,4] :count minuty|[5,Inf] :count minut',
 
