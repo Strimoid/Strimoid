b03703a80acc033710067c19605d99a55cf179ab
duxet <duxetlg@gmail.com>
2014-10-23 20:25:37 +0200

    Add EntryTableSeeder

--- app/database/seeds/EntryTableSeeder.php
+++ app/database/seeds/EntryTableSeeder.php
@@ -1,10 +1,10 @@
 <?php
 
-class ContentTableSeeder extends Seeder {
+class EntryTableSeeder extends Seeder {
 
     public function run()
     {
-        DB::collection('contents')->delete();
+        DB::collection('entries')->delete();
 
         // Get list of user and group ids
         $groupIds = DB::collection('groups')->lists('_id');
@@ -21,10 +21,8 @@ class ContentTableSeeder extends Seeder {
             Content::create([
                 'created_at' => $faker->dateTimeThisDecade,
                 'group_id' => $randomGroup,
-                'title' => $faker->sentence(10),
-                'description' => $faker->text(200),
+                'text' => $faker->text(512),
                 'user_id' => $randomUser,
-                'url' => $faker->url,
             ]);
         }
     }
