8e7c9538dedda02b15f15cb7a2af9ef076e77b70
duxet <duxetlg@gmail.com>
2014-11-01 19:47:55 +0100

    Fix

--- app/utils.php
+++ app/utils.php
@@ -1,112 +1,133 @@
 <?php
 
-function shadow($text)
+if ( ! function_exists('shadow'))
 {
-    $text = Str::ascii($text);
-    $text = Str::lower($text);
-
-    return $text;
-}
-
-function hash_email($email)
-{
-    return hash('sha384', Config::get('app.email_salt') . $email . md5($email));
-}
-
-// Change "@username" and "g/groupname" into markdown links
-function parse_usernames($body)
-{
-    $body = preg_replace_callback('/(?<=^|\s)c\/([a-z0-9_-]+)(?=$|\s|:|.)/i', function($matches) {
-        $content = Content::find($matches[1]);
-
-        if ($content)
-        {
-            return '['. str_replace('_', '\_', $content->title) . ']('. $content->getSlug() .')';
-        }
-        else
-        {
-            return 'c/'. $matches[1];
-        }
-    }, $body);
-
-    $body = preg_replace_callback('/(?<=^|\s)u\/([a-z0-9_-]+)(?=$|\s|:|.)/i', function($matches) {
-        $target = User::where('shadow_name', Str::lower($matches[1]))->first();
-
-        if ($target)
-        {
-            return '[u/' . str_replace('_', '\_', $target->name) . '](/u/' . $target->name . ')';
-        }
-        else
-        {
-            return 'u/'. $matches[1];
-        }
-    }, $body);
-
-    $body = preg_replace_callback('/(?<=^|\s)@([a-z0-9_-]+)(?=$|\s|:|.)/i', function($matches) {
-        $target = User::where('shadow_name', Str::lower($matches[1]))->first();
-
-        if ($target)
-        {
-            return '[@' . str_replace('_', '\_', $target->name) . '](/u/' . $target->name . ')';
-        }
-        else
-        {
-            return '@'. $matches[1];
-        }
-    }, $body);
-
-    $body = preg_replace_callback('/(?<=^|\s)(?<=\s|^)g\/([a-z0-9_-żźćńółęąśŻŹĆĄŚĘŁÓŃ]+)(?=$|\s|:|.)/i', function($matches) {
-        $groupName = shadow($matches[1]);
-
-        $target = Group::where('shadow_urlname', $groupName)->first();
-        $fakeGroup = class_exists('Groups\\'. studly_case($groupName));
-
-        if ($target || $fakeGroup)
-        {
-            $urlname = $target ? $target->urlname : $groupName;
-
-            return '[g/' . str_replace('_', '\_', $urlname) . '](/g/' . $urlname . ')';
-        }
-        else
-        {
-            return 'g/'. $matches[1];
-        }
-    }, $body);
-
-    return $body;
-}
-
-// Convert MongoDate to Carbon
-function md_to_carbon(MongoDate $md)
-{
-    return Carbon::createFromTimestamp($md->sec);
-}
-
-function carbon_to_md(Carbon $c)
-{
-    return new MongoDate($c->timestamp);
-}
-
-// Convert MongoID to Base58
-function mid_to_b58($mongoId)
-{
-    return gmp_strval(gmp_init($mongoId, 16), 58);
-}
-
-function b58_to_mid($base58)
-{
-    return gmp_strval(gmp_init($base58, 58), 16);
-}
-
-// Convert to boolean
-function toBool($var) {
-    $lower = strtolower($var);
-
-    if ($var === true || $lower == 'on' || $lower == 'true')
+    function shadow($text)
     {
-        $result = true;
-    }
+        $text = Str::ascii($text);
+        $text = Str::lower($text);
 
-    return (isset($result) && $result) ?: false;
+        return $text;
+    }
 }
+
+if ( ! function_exists('hash_email')) {
+    function hash_email($email)
+    {
+        return hash('sha384', Config::get('app.email_salt') . $email . md5($email));
+    }
+}
+
+if ( ! function_exists('parse_usernames')) {
+
+    // Change "@username" and "g/groupname" into markdown links
+    function parse_usernames($body)
+    {
+        $body = preg_replace_callback('/(?<=^|\s)c\/([a-z0-9_-]+)(?=$|\s|:|.)/i', function($matches) {
+            $content = Content::find($matches[1]);
+
+            if ($content)
+            {
+                return '['. str_replace('_', '\_', $content->title) . ']('. $content->getSlug() .')';
+            }
+            else
+            {
+                return 'c/'. $matches[1];
+            }
+        }, $body);
+
+        $body = preg_replace_callback('/(?<=^|\s)u\/([a-z0-9_-]+)(?=$|\s|:|.)/i', function($matches) {
+            $target = User::where('shadow_name', Str::lower($matches[1]))->first();
+
+            if ($target)
+            {
+                return '[u/' . str_replace('_', '\_', $target->name) . '](/u/' . $target->name . ')';
+            }
+            else
+            {
+                return 'u/'. $matches[1];
+            }
+        }, $body);
+
+        $body = preg_replace_callback('/(?<=^|\s)@([a-z0-9_-]+)(?=$|\s|:|.)/i', function($matches) {
+            $target = User::where('shadow_name', Str::lower($matches[1]))->first();
+
+            if ($target)
+            {
+                return '[@' . str_replace('_', '\_', $target->name) . '](/u/' . $target->name . ')';
+            }
+            else
+            {
+                return '@'. $matches[1];
+            }
+        }, $body);
+
+        $body = preg_replace_callback('/(?<=^|\s)(?<=\s|^)g\/([a-z0-9_-żźćńółęąśŻŹĆĄŚĘŁÓŃ]+)(?=$|\s|:|.)/i', function($matches) {
+            $groupName = shadow($matches[1]);
+
+            $target = Group::where('shadow_urlname', $groupName)->first();
+            $fakeGroup = class_exists('Groups\\'. studly_case($groupName));
+
+            if ($target || $fakeGroup)
+            {
+                $urlname = $target ? $target->urlname : $groupName;
+
+                return '[g/' . str_replace('_', '\_', $urlname) . '](/g/' . $urlname . ')';
+            }
+            else
+            {
+                return 'g/'. $matches[1];
+            }
+        }, $body);
+
+        return $body;
+    }
+}
+
+if ( ! function_exists('md_to_carbon')) {
+    // Convert MongoDate to Carbon
+    function md_to_carbon(MongoDate $md)
+    {
+        return Carbon::createFromTimestamp($md->sec);
+    }
+}
+
+
+if ( ! function_exists('carbon_to_md')) {
+    function carbon_to_md(Carbon $c)
+    {
+        return new MongoDate($c->timestamp);
+    }
+}
+
+if ( ! function_exists('mid_to_b58')) {
+    // Convert MongoID to Base58
+    function mid_to_b58($mongoId)
+    {
+        return gmp_strval(gmp_init($mongoId, 16), 58);
+    }
+}
+
+if ( ! function_exists('b58_to_mid')) {
+    function b58_to_mid($base58)
+    {
+        return gmp_strval(gmp_init($base58, 58), 16);
+    }
+}
+
+if ( ! function_exists('toBool')) {
+    // Convert to boolean
+    function toBool($var) {
+        $lower = strtolower($var);
+
+        if ($var === true || $lower == 'on' || $lower == 'true')
+        {
+            $result = true;
+        }
+
+        return (isset($result) && $result) ?: false;
+    }
+}
+
+
 
