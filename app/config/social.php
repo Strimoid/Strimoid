127eede375f30c879eb792f8c925cac9acaf8a06
duxet <duxetlg@gmail.com>
2014-11-01 23:19:11 +0100

    Update config

--- app/config/social.php
+++ app/config/social.php
@@ -2,12 +2,12 @@
 
 return [
     'facebook' => [
-        'page_token'      => ''
+        'page_token'      => $_ENV['fb_page_token'],
     ],
     'twitter' => [
-        'consumer_key'    => '',
-        'consumer_secret' => '',
-        'token'           => '',
-        'token_secret'    => ''
+        'consumer_key'    => $_ENV['twitter_ck'],
+        'consumer_secret' => $_ENV['twitter_cs'],
+        'token'           => $_ENV['twitter_t'],
+        'token_secret'    => $_ENV['twitter_ts'],
     ]
 ];
