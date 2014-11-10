ee178b63a987feebeb488f1fcfb55261e98ac488
duxet <duxetlg@gmail.com>
2014-11-10 23:01:32 +0100

    Add notifications to retsult of api /login method

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -22,11 +22,17 @@ class AuthController extends BaseController {
                 $folder['groups'] = Group::whereIn('_id', $folder['groups'])->get()->toArray();
             }
 
+            $notifications = Notification::with(['sourceUser' => function($q) { $q->select('avatar')->remember(3); }])
+                ->target(['user_id' => Auth::id()])
+                ->orderBy('created_at', 'desc')
+                ->take(15)->get();
+
             $data['user'] = array_merge(Auth::user()->toArray(), [
                 'subscribed_groups' => Group::whereIn('_id', Auth::user()->subscribedGroups())->get()->toArray(),
                 'blocked_groups' => Group::whereIn('_id', Auth::user()->blockedGroups())->get()->toArray(),
                 'moderated_groups' => Group::whereIn('_id', Auth::user()->moderatedGroups())->get()->toArray(),
                 'folders' => $folders,
+                'notifications' => $notifications,
             ]);
 
             return Response::json($data);
