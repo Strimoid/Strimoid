d3498d068e04266853cb3dc81f3d15071c2ad650
duxet <duxetlg@gmail.com>
2014-11-10 17:36:19 +0100

    Fix CSRF filter

--- app/filters.php
+++ app/filters.php
@@ -142,7 +142,7 @@ Route::filter('csrf', function()
 {
     $token = Request::ajax() ? Request::header('X-CSRF-Token') : Input::get('_token');
 
-	if (Session::token() != $token)
+	if (Session::token() !== $token)
 	{
 		throw new Illuminate\Session\TokenMismatchException;
 	}
