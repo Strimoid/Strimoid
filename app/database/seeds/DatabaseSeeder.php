9f333467a2103c8f35f8c6eb97e964c87cb3df3e
duxet <duxetlg@gmail.com>
2014-11-12 18:14:48 +0100

    Run EntryTableSeeder to list

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('UserTableSeeder');
 		$this->call('GroupTableSeeder');
 		$this->call('ContentTableSeeder');
+		$this->call('EntryTableSeeder');
 	}
 
 }
