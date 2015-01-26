c339df35d68658f8b77bb936799160156b12e4ee
duxet <duxetlg@gmail.com>
2015-01-26 19:44:17 +0100

    Enable RateLimit middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends HttpKernel {
         'Illuminate\Session\Middleware\StartSession',
         'Illuminate\View\Middleware\ShareErrorsFromSession',
         'Illuminate\Foundation\Http\Middleware\VerifyCsrfToken',
+        'Strimoid\Http\Middleware\RateLimit',
     ];
     /**
      * The application's route middleware.
