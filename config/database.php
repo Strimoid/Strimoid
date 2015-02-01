6c28c9023d92fcdc3238eaff1e3e926637cd993c
duxet <duxetlg@gmail.com>
2015-02-01 16:36:25 +0100

    Add credentials to db config

--- config/database.php
+++ config/database.php
@@ -38,6 +38,8 @@ return [
             'host'     => env('DB_HOST', 'localhost'),
             'port'     => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE', 'default'),
+			'username' => env('DB_USERNAME'),
+			'password' => env('DB_PASSWORD'),
         ],
 
         'stats' => [
