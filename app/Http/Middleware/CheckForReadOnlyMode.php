07e79adf711854250c8c4e5236bfb30b26981e8a
duxet <duxetlg@gmail.com>
2015-02-17 23:54:20 +0100

    Add read only mode

---
+++ app/Http/Middleware/CheckForReadOnlyMode.php
@@ -0,0 +1,46 @@
+<?php namespace Strimoid\Http\Middleware; 
+
+use Illuminate\Contracts\Foundation\Application;
+
+class CheckForReadOnlyMode {
+
+    /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->getMethod() != 'GET' && $this->isReadOnlyModeEnabled())
+        {
+            throw new HttpException(503);
+        }
+
+        return $next($request);
+    }
+
+    public function isReadOnlyModeEnabled()
+    {
+        return file_exists($this->app->storagePath().'/framework/readonly');
+    }
+
+}
