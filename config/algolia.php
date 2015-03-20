f650af97192ab0124679e9238be3b462c81e2180
duxet <duxetlg@gmail.com>
2015-03-20 16:05:07 +0100

    Add Algolia config

---
+++ config/algolia.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Algolia Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'id'  => env('ALGOLIA_ID', 'your-application-id'),
+            'key' => env('ALGOLIA_KEY', 'your-api-key'),
+        ],
+
+    ]
+
+];
