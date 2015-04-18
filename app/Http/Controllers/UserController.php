a5308c2f2d3a4b8cf5add8010a2d72f1b06ac70a
duxet <duxetlg@gmail.com>
2015-04-18 17:24:16 +0200

    Fix user profile view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -317,12 +317,12 @@ class UserController extends BaseController
         } elseif ($type == 'comments') {
             $data['comments'] = $user->comments()->orderBy('created_at', 'desc')->paginate(15);
         } elseif ($type == 'comment_replies') {
-            $data['actions'] = $user->actions()->where('type', CommentReply::class)
+            $data['actions'] = $user->actions()->where('element_type', CommentReply::class)
                 ->orderBy('created_at', 'desc')->paginate(15);
         } elseif ($type == 'entries') {
             $data['entries'] = $user->entries()->orderBy('created_at', 'desc')->paginate(15);
         } elseif ($type == 'entry_replies') {
-            $data['actions'] = $user->actions()->where('type', EntryReply::class)
+            $data['actions'] = $user->actions()->where('element_type', EntryReply::class)
                 ->orderBy('created_at', 'desc')->paginate(15);
         } elseif ($type == 'moderated') {
             $data['moderated'] = $user->moderatedGroups();
