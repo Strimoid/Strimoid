00736b401819007ebc92a2c578a1f199fe0032a8
duxet <duxetlg@gmail.com>
2015-04-18 11:10:21 +0200

    Fix notification target

--- app/Models/NotificationTarget.php
+++ app/Models/NotificationTarget.php
@@ -1,26 +1,15 @@
 <?php namespace Strimoid\Models;
 
-/**
- * Strimoid\Models\NotificationTarget
- *
- * @property-read \Strimoid\Models\User $user 
- * @property-write mixed $read 
- * @property-read mixed $vote_state 
- * @property-read \Illuminate\Database\Eloquent\Collection|Vote[] $vote 
- * @property-read \Illuminate\Database\Eloquent\Collection|Save[] $usave 
- * @method static \Strimoid\Models\BaseModel fromDaysAgo($days)
- */
+use Strimoid\Models\Traits\HasUserRelationship;
+
 class NotificationTarget extends BaseModel
 {
+    use HasUserRelationship;
+
     protected $attributes = ['read' => false];
 
-    public function user()
+    public function notification()
     {
-        return $this->belongsTo('Strimoid\Models\User');
-    }
-
-    public function setReadAttribute($value)
-    {
-        $this->attributes['read'] = toBool($value);
+        return $this->belongsTo(Notification::class);
     }
 }
