d88a4ad79c331c3977a2df95ccf82c79a14bef22
duxet <duxetlg@gmail.com>
2015-04-20 21:55:33 +0200

    Add Redis configuration

--- config/database.php
+++ config/database.php
@@ -66,4 +66,27 @@ return [
 
     'migrations' => 'migrations',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Redis Databases
+    |--------------------------------------------------------------------------
+    |
+    | Redis is an open source, fast, and advanced key-value store that also
+    | provides a richer set of commands than a typical key-value systems
+    | such as APC or Memcached. Laravel makes it easy to dig right in.
+    |
+    */
+
+    'redis' => [
+
+        'cluster' => false,
+
+        'default' => [
+            'host'     => '127.0.0.1',
+            'port'     => 6379,
+            'database' => 0,
+        ],
+
+    ],
+
 ];
