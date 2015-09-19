1c25a1b0d662eecb94130e325171e01970bb1545
duxet <duxetlg@gmail.com>
2015-09-20 01:53:47 +0200

    Add deploy.php

---
+++ deploy.php
@@ -0,0 +1,13 @@
+<?php // https://github.com/deployphp/deployer
+
+require 'recipe/laravel.php';
+
+// Define a server for deployment.
+server('box', getenv('SEMAPHORE_SERVER_NAME'))
+    ->user('strimoid')
+    ->identityFile()
+    ->stage('staging')
+    ->env('deploy_path', '/www/strimoid'); // Define the base path to deploy your project to.
+
+set('ssh_type', 'ext-ssh2');
+set('repository', 'https://github.com/Strimoid/Strimoid.git');
