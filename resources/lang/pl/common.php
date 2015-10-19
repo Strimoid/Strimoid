bcdc62e2857411927999d642c219fc908370856f
duxet <duxetlg@gmail.com>
2015-10-19 22:37:08 +0200

    Add missing translations for Polish

--- resources/lang/pl/common.php
+++ resources/lang/pl/common.php
@@ -25,4 +25,30 @@ return [
     'night mode' => 'tryb nocny',
     'website uses' => 'Serwis wykorzystuje',
     'cookie files' => 'pliki cookies',
+
+    // Sidebars
+    'popular contents' => 'Popularne treści',
+    'popular comments' => 'Popularne komentarze',
+    'stats' => 'Statystyki',
+
+    // Actions
+    'add' => 'Dodaj',
+    'add comment' => 'Dodaj komentarz',
+    'add link' => 'Dodaj link',
+    'add content' => 'Dodaj treść',
+    'block' => 'Zablokuj',
+    'unblock' => 'Odblokuj',
+
+    // Things
+    'comments' => 'Komentarze',
+    'group' => 'Grupa',
+    'folders' => 'Foldery',
+
+    // Other
+    'description' => 'Opis',
+    'title' => 'Tytuł',
+    'recently popular' => 'Ostatnio popularne',
+    'sort' => 'Sortowanie',
+    'filter' => 'Filtrowanie',
+    'url address' => 'Adres URL',
 ];
