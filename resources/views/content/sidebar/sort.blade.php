46bff325ad2e494c00680465d6949ca2d6123c2d
duxet <duxetlg@gmail.com>
2016-02-14 15:16:12 +0100

    Dropdown fix

--- resources/views/content/sidebar/sort.blade.php
+++ resources/views/content/sidebar/sort.blade.php
@@ -6,17 +6,17 @@
                 @lang('common.sort')
                 <span class="caret"></span>
             </button>
-            <ul class="dropdown-menu content_sort">
-                <li>
-                    <a class="action_link @if (!Input::has('sort')) selected @endif" data-sort="">Domyślne</a>
-                </li>
-                <li>
-                    <a class="action_link @if (Input::get('sort') == 'uv') selected @endif" data-sort="uv">Liczba UV</a>
-                </li>
-                <li>
-                    <a class="action_link @if (Input::get('sort') == 'comments') selected @endif" data-sort="comments">Liczba komentarzy</a>
-                </li>
-            </ul>
+            <div class="dropdown-menu content_sort">
+                <a class="dropdown-item action_link @if (!Input::has('sort')) selected @endif" data-sort="">
+                    Domyślne
+                </a>
+                <a class="dropdown-item action_link @if (Input::get('sort') == 'uv') selected @endif" data-sort="uv">
+                    Liczba UV
+                </a>
+                <a class="dropdown-item action_link @if (Input::get('sort') == 'comments') selected @endif" data-sort="comments">
+                    Liczba komentarzy
+                </a>
+            </div>
         </div>
 
         <div class="btn-group half-width">
@@ -25,20 +25,21 @@
                 @lang('common.filter')
                 <span class="caret"></span>
             </button>
-            <ul class="dropdown-menu content_filter">
-                <li>
-                    <a class="action_link @if (!Input::has('time')) selected @endif" data-time="">Wszystkie</a>
-                </li>
-                <li>
-                    <a class="action_link @if (Input::get('time') == '1d') selected @endif" data-time="1d">Z ostatnich 24 godzin</a>
-                </li>
-                <li>
-                    <a class="action_link @if (Input::get('time') == '5d') selected @endif" data-time="5d">Z ostatnich 5 dni</a>
-                </li>
-                <li>
-                    <a class="action_link @if (Input::get('time') == '30d') selected @endif" data-time="30d">Z ostatnich 30 dni</a>
-                </li>
-            </ul>
+            <div class="dropdown-menu content_filter">
+                <a class="dropdown-item action_link @if (!Input::has('time')) selected @endif" data-time="">Wszystkie</a>
+
+                <a class="dropdown-item action_link @if (Input::get('time') == '1d') selected @endif" data-time="1d">
+                    Z ostatnich 24 godzin
+                </a>
+
+                <a class="dropdown-item action_link @if (Input::get('time') == '5d') selected @endif" data-time="5d">
+                    Z ostatnich 5 dni
+                </a>
+
+                <a class="dropdown-item action_link @if (Input::get('time') == '30d') selected @endif" data-time="30d">
+                    Z ostatnich 30 dni
+                </a>
+            </div>
         </div>
     </div>
 </div>
