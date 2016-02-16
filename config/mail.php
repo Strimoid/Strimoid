08a1aa4e9aa131ed419984d1474d561f48a1fc79
duxet <duxetlg@gmail.com>
2016-02-16 20:00:02 +0100

    Login form css fix

--- config/mail.php
+++ config/mail.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'encryption' => 'tls',
+    'encryption' => env('SMTP_ENCRYPTION', 'tls'),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,6 +119,6 @@ return [
     |
     */
 
-    'pretend' => false,
+    'pretend' => env('MAIL_PRETEND', false),
 
 ];
