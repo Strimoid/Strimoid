5807e845e60a2202fca363717c73aaf1e1708145
duxet <duxetlg@gmail.com>
2016-04-18 19:49:02 +0200

    Fix tests

--- resources/views/content/sidebar/author_panel.blade.php
+++ resources/views/content/sidebar/author_panel.blade.php
@@ -6,7 +6,7 @@
         <a href="{!! action('ContentController@showEditForm', $content) !!}" class="btn btn-sm btn-secondary">
             Edytuj treść
         </a>
-        <a href="{!! action('ContentController@chooseThumbnail', $content) !!}" class="btn btn-sm btn-secondary">
+        <a href="{!! action('Content\ThumbnailController@chooseThumbnail', $content) !!}" class="btn btn-sm btn-secondary">
             Zmień miniaturkę
         </a>
         <a class="btn btn-sm btn-danger content_remove_btn" data-id="{!! $content->hashId() !!}">
