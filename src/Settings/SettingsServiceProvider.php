13d8a784dc0805713045dcacd39adad027320a40
duxet <duxetlg@gmail.com>
2016-05-10 20:35:28 +0200

    Update .env.sample

---
+++ src/Settings/SettingsServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Strimoid\Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
