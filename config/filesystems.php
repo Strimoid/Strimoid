6347ef01ac4fc85a2e9359a69b374fd1501be8e9
duxet <duxetlg@gmail.com>
2016-06-25 20:18:09 +0200

    Fixes

--- config/filesystems.php
+++ config/filesystems.php
@@ -11,10 +11,23 @@ return [
     | by the framework. A "local" driver, as well as a variety of cloud
     | based drivers are available for your choosing. Just store away!
     |
-    | Supported: "local", "s3", "rackspace"    |
+    | Supported: "local", "ftp", "s3", "rackspace"
     */
 
     'default' => 'local',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
+
+    'cloud' => 's3',
 
     /*
     |--------------------------------------------------------------------------
