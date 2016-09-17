a27b7054e4e57f128f3948f61215d2a3f86fd211
duxet <duxetlg@gmail.com>
2016-09-17 21:06:26 +0200

    Add missing auth translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -3,4 +3,11 @@
 return [
     'invalid_credentials' => 'Incorrect user name or password.',
     'logged_out'          => 'You have been logged out.',
+    'remember'            => 'remember me',
+    'username'            => 'username',
+    'password'            => 'hasło',
+    'email'               => 'password',
+    'register'            => 'sign up',
+    'registration'        => 'sign up',
+    'logging in'          => 'log in',
 ];
