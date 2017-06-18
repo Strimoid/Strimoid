bc93f071a3ae098a5e734b44bfe25199851f11b3
duxet <duxetlg@gmail.com>
2017-06-18 19:22:00 +0200

    Fix sign up form

--- resources/views/global/form/input_icon.blade.php
+++ resources/views/global/form/input_icon.blade.php
@@ -14,7 +14,7 @@ $options = [
 
 ?>
 
-<div class="form-group @if ($errors->has($name)) has-error @endif">
+<div class="row form-group @if ($errors->has($name)) has-error @endif">
     <label for="{!! $name !!}" class="col-lg-3 control-label">{!! $label !!}</label>
 
     <div class="col-lg-6">
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,7 +1,7 @@
 @extends('global.master')
 
 @section('title')
-Rejestracja
+    {{ Str::ucfirst(trans('auth.registration')) }}
 @stop
 
 @section('content')
@@ -28,15 +28,3 @@ Rejestracja
     {!! Form::close() !!}
 </div>
 @stop
-
-{{--
-@section('sidebar')
-
-<div class="well">
-    <h4>Dlaczego warto się zarejestrować?</h4>
-    <p>Dołączenie do społeczności {!! config('app.site_name') !!} pozwoli Ci na pełny udział w życiu serwisu
-        oraz możliwość dostosowania go do własnych upodobań.</p>
-    <p>Zapraszamy!</p>
-</div>
-@stop
---}}
