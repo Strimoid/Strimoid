ddc0fab5b5c17f14c4745c14312745f88077d23d
duxet <duxetlg@gmail.com>
2020-01-18 10:30:36 +0100

    Fix DB migrations

--- database/migrations/2015_03_15_000002_create_groups_table.php
+++ database/migrations/2015_03_15_000002_create_groups_table.php
@@ -18,7 +18,7 @@ class CreateGroupsTable extends Migration
             // Basic data
             $table->string('avatar', 16)->nullable();
             $table->string('description', 255)->nullable();
-            $table->string('name')->unique()->collation = 'utf8_unicode_ci';
+            $table->string('name')->unique();
             $table->string('style')->nullable();
             $table->string('urlname');
             $table->enum('type', ['public', 'private']);
