287479fd23023f36f7eac37757626037d3fc0b2d
duxet <duxetlg@gmail.com>
2020-03-23 16:39:54 +0100

    Fix content vote buttons

--- resources/views/content/widget.blade.php
+++ resources/views/content/widget.blade.php
@@ -1,10 +1,15 @@
  <div class="media content" data-id="{!! $content->hashId() !!}">
-        <vote-buttons class="voting"
-                      uv="{!! $content->uv !!}"
-                      dv="{!! $content->dv !!}"
-                      data-id="{!! $content->hashId() !!}"
-                      state="{!! $content->getVoteState()  !!}"
-                      data-type="content"></vote-buttons>
+     <span class="voting" data-id="{!! $content->hashId() !!}" state="{!! $content->getVoteState() !!}" data-type="content">
+        <button type="button" class="btn btn-default btn-sm vote-btn-up @if ($content->getVoteState() == 'uv') btn-success @endif">
+            <i class="fa fa-arrow-up vote-up"></i>
+            <span class="count">{!! $content->uv !!}</span>
+        </button>
+
+        <button type="button" class="btn btn-default btn-sm vote-btn-down @if ($content->getVoteState() == 'dv') btn-danger @endif">
+            <i class="fa fa-arrow-down vote-down"></i>
+            <span class="count">{!! $content->dv !!}</span>
+        </button>
+    </span>
 
         @if ($content->thumbnail && !$content->nsfw)
         <a class="pull-left" href="{{ $content->getURL() }}" rel="nofollow" target="_blank">
