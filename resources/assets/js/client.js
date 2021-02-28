7b266c89df53907d5c51fb73be7d24e3e6b356ea
duxet <duxetlg@gmail.com>
2021-02-28 09:36:10 +0100

    Fix webpack build

--- resources/assets/js/client.js
+++ resources/assets/js/client.js
@@ -1,5 +1,5 @@
-require('expose-loader?exposes[]=_!lodash')
-require('expose-loader?exposes[]=$&exposes[]=jQuery!jquery')
+require('expose-loader?exposes=_!lodash')
+require('expose-loader?exposes=$,jQuery!jquery')
 // require('expose-loader?Pjax!pjax')
 // require('expose-loader?React!react')
 // require('expose-loader?ReactDOM!react-dom')
